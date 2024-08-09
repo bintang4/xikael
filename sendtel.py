b8291f78167cb4016a670673a5c71b930c51b372
bintang4 <43073401+bintang4@users.noreply.github.com>
2024-08-09 10:44:58 +0700

    Add files via upload

---
+++ sendtel.py
@@ -0,0 +1,14 @@
+import re,os
+import requests
+
+def send_telegram_file(file_path, caption):
+    url = f"https://api.telegram.org/bot6258586547:AAFhXNBPJDCRQJ-1Mmj0OkSG3mHvmAbmk-0/sendDocument"
+    with open(file_path, 'rb') as file:
+        files = {'document': file}
+        data = {'chat_id': 5981225273, 'caption': caption}
+        response = requests.post(url, data=data, files=files)
+    return response
+
+print(" TOOLS SEND FILE TO TELEGRAM ")
+filees = input("Files -> ")
+send_telegram_file(filees)
